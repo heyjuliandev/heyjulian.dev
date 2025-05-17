8f1e6d8b20fab742de6a4c1870c55ddfebc9cb37
Julián Chávez Burruezo <jchavezbur@gmail.com>
2025-05-17 01:43:49 +0100

    feat: vercel added for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
+  integrations: [tailwind()],
+  adapter: vercel(),
   vite: {
     resolve: {
       alias: {
